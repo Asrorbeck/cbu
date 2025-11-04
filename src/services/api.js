e54133f77731c93e0704a4769a6c721ceac263e5
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2025-11-04 23:51:35 +0500

    api changed

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
-  "https://jonibekdaminov.pythonanywhere.com/api/v1";
+  "https://bd242f6369a3.ngrok-free.app/api/v1";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
